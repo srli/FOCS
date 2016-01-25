0b0e633c1e846e2be38c6106b780e9bbbb42ec43
srli <sophia.li@students.olin.edu>
2016-01-24 19:14:31 -0500

    adding hw1 base files

---
+++ hw1/homework1.ml
@@ -0,0 +1,94 @@
+(* 
+
+HOMEWORK 1
+
+Name: Sophie
+
+Email: sophia.li@students.olin.edu
+
+Remarks, if any:
+
+*)
+
+
+(*
+ *
+ * Please fill in this file with your solutions and submit it
+ *
+ * The functions below are stubs that you should replace with your
+ * own implementation.
+ *
+ * Always make sure you can #use this file before submitting it.
+ * It has to load without any errors.
+ *
+ *)
+
+
+
+(* Question 1 *)
+
+let gcd (a,b) = 
+   failwith "not implemented"
+
+
+let is_coprime (a,b) = 
+   failwith "not implemented"
+
+
+let euler (n) = 
+   failwith "not implemented"
+
+
+let coprimes (n) = 
+   failwith "not implemented"
+
+
+
+(* Question 2 *)
+
+let append (xs,ys) = 
+   failwith "not implemented"
+
+
+let flatten (xss) = 
+   failwith "not implemented"
+
+
+let nth (n,xs) = 
+   failwith "not implemented"
+
+
+let last (xs) = 
+   failwith "not implemented"
+
+
+let separate (xs) = 
+   failwith "not implemented"
+
+
+
+(* Question 3 *)
+
+let setIn (e,xs) = 
+   failwith "not implemented"
+
+
+let setSub (xs,ys) = 
+   failwith "not implemented"
+
+
+let setEqual (xs,ys) = 
+   failwith "not implemented"
+
+
+let setUnion (xs,ys) = 
+   failwith "not implemented"
+
+
+let setInter (xs,ys) = 
+   failwith "not implemented"
+
+
+let setSize (xs) = 
+   failwith "not implemented"
+
