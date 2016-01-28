4917236dd1a41fa759b3e8642f6905dee53846a3
srli <c>
2016-01-27 22:51:20 -0500

    more of pt2

--- hw1/homework1.ml
+++ hw1/homework1.ml
@@ -35,13 +35,13 @@ let min (a, b) =
 let rec gcd (a,b) = 
    if abs(a - b) > 0 then
       gcd(abs(a - b), min(a, b))
-   else if (a - b) == 0 then
+   else if (a - b) = 0 then
       a
    else
       failwith "gcd failure"
 
 let is_coprime (a,b) = 
-   if gcd (a, b) == 1 then
+   if gcd (a, b) = 1 then
       true
    else
       false
@@ -97,28 +97,55 @@ let rec nth (xs, n) =
    match n with
    | [] -> failwith "out of bounds"
    | hd :: tl -> 
-      if xs > 1 then
+      if xs > 0 then
          nth(xs - 1, tl)
       else
          hd
 
-let separate (xs) = 
-   failwith "not implemented"
-
+let rec separate (xs) = 
+   match xs with
+   | [] -> ([], [])
+   | (x, y) :: (tl) -> tl(* x::separate(tl), y::separate(tl) *)
 
 
 (* Question 3 *)
 
-let setIn (e,xs) = 
-   failwith "not implemented"
+let rec setIn (e,xs) = 
+   match xs with
+   | [] -> false
+   | hd :: tl ->
+      if hd = e then
+         true
+      else
+         setIn(e, tl)
 
 
-let setSub (xs,ys) = 
-   failwith "not implemented"
+let rec setSub_helper(x, ys) =
+   match ys with
+   | [] -> false
+   | hd :: tl ->
+      if hd = x then
+         true
+      else
+         setSub_helper(x, tl) 
 
+let rec setSub (xs,ys) = 
+   match xs with
+   | [] -> true
+   | hd :: tl ->
+      if setSub_helper(hd, ys) then
+         setSub(tl, ys)
+      else
+         false
 
 let setEqual (xs,ys) = 
-   failwith "not implemented"
+   if setSub(xs, ys) then
+      if setSub(ys, xs) then
+         true
+      else
+         false
+   else
+      false
 
 
 let setUnion (xs,ys) = 
