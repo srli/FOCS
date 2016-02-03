1ccd85f8c1d9f8dbac1f60cadd931d8075fdd835
srli <sophia.li@students.olin.edu>
2016-02-02 19:37:59 -0500

    hw2

--- hw2/homework2.ml
+++ hw2/homework2.ml
@@ -1,4 +1,4 @@
-(* 
+(*
 
 HOMEWORK 2
 
@@ -11,7 +11,7 @@ Remarks, if any:
 Notes:
 
 | -> or
-+ -> 
++ ->
 
 *)
 
@@ -32,10 +32,10 @@ Notes:
 
 (* QUESTION 1 *)
 
-let rec prepend (letter, lang) = 
+let rec prepend (letter: string) (lang: string list) =
   match lang with
   | [] -> "0"
-  | hd :: tl -> letter^hd @ prepend(letter, tl);;
+  | hd :: tl -> letter^hd@prepend(letter, tl);;
 
 
 let concatenate (alphabet, lang) = failwith "not implemented"
@@ -45,7 +45,7 @@ let all_strings (alphabet, n) = failwith "not implemented"
 
 
 
-(* QUESTION 2 *)
+(* (* QUESTION 2 *)
 
 let restrict (xs,n) = failwith "not implemented"
 
@@ -57,98 +57,98 @@ let langConcat (xs,ys,n) = failwith "not implemented"
 
 
 let langStar (xs,n) = failwith "not implemented"
+ *)
 
 
-
-(* QUESTION 3 *)
+(* (* QUESTION 3 *)
 
 
 (* some helper code -- vaguely advanced OCaml in here, but barely *)
 
 type re = Empty | Unit | Letter of string | Plus of re * re | Times of re * re | Star of re
 
-let lang (s,n) = 
+let lang (s,n) =
   let fromChar c = String.make 1 c in
-  let explode s = 
-    let rec loop i result = 
+  let explode s =
+    let rec loop i result =
       if i < 0 then result
       else loop (i-1) (s.[i]::result) in
     loop (String.length s - 1) []  in
-  (* Grammar: 
+  (* Grammar:
    *
    * R ::= R1 + R
    *       R1
-   * 
+   *
    * R1 ::= R2 R1
    *        R2
-   * 
+   *
    * R2 ::= R3*
    *        R3
-   * 
+   *
    * R3 ::= a
    *        1
-   *        0 
+   *        0
    *        ( R )
    *)
   let isalpha = function 'A'..'Z'|'a'..'z' -> true | _ -> false in
-  let expect c cs = 
-    match cs with 
+  let expect c cs =
+    match cs with
       f::cs when f = c -> Some cs
     | _ -> None in
-  let expect_alpha cs = 
+  let expect_alpha cs =
     match cs with
       f::cs when isalpha f -> Some (f,cs)
     | _ -> None  in
-  let rec parse_R cs = 
+  let rec parse_R cs =
     match parse_R1 cs with
       None -> None
-    | Some (r1,cs) -> 
+    | Some (r1,cs) ->
         (match expect '+' cs with
            None -> Some (r1,cs)
-         | Some cs -> 
+         | Some cs ->
              (match parse_R cs with
                 None -> None
               | Some (r2,cs) -> Some (Plus(r1,r2),cs)))
-  and parse_R1 cs = 
+  and parse_R1 cs =
     match parse_R2 cs with
       None -> None
-    | Some (r1,cs) -> 
+    | Some (r1,cs) ->
         (match parse_R1 cs with
            None -> Some (r1,cs)
-         | Some (r2,cs) -> Some (Times(r1,r2),cs))  
-  and parse_R2 cs = 
+         | Some (r2,cs) -> Some (Times(r1,r2),cs))
+  and parse_R2 cs =
     match parse_R3 cs with
       None -> None
-    | Some (r1,cs) -> 
+    | Some (r1,cs) ->
         (match expect '*' cs with
            None -> Some (r1,cs)
          | Some cs -> Some (Star(r1),cs))
-  and parse_R3 cs = 
+  and parse_R3 cs =
     match expect_alpha cs with
       Some (a,cs) -> Some (Letter(fromChar(a)),cs)
-    | None -> 
+    | None ->
         (match expect '1' cs with
            Some cs -> Some (Unit, cs)
-         | None -> 
+         | None ->
              (match expect '0' cs with
                 Some cs -> Some (Empty,cs)
               | None -> parse_parens cs))
-  and parse_parens cs = 
+  and parse_parens cs =
     match expect '(' cs with
       None -> None
-    | Some cs -> 
+    | Some cs ->
         (match parse_R cs with
            None -> None
-         | Some (r,cs) -> 
+         | Some (r,cs) ->
              (match expect ')' cs with
                 None -> None
               | Some cs -> Some (r,cs)))  in
-  let parse s = 
+  let parse s =
     let cs = explode s in
     match parse_R cs with
       Some (re,[]) -> re
     | _ -> failwith ("Cannot parse "^s)  in
-  let rec eval re = 
+  let rec eval re =
     match re with
       Empty -> []
     | Unit -> [""]
@@ -158,8 +158,8 @@ let lang (s,n) =
     | Star r -> langStar(eval r,n)  in
     eval (parse s)
 
-let dump l = 
-  List.iter (fun s -> match s with "" -> print_string "  <empty>\n" 
+let dump l =
+  List.iter (fun s -> match s with "" -> print_string "  <empty>\n"
                                  | s -> print_string ("  "^s^"\n")) l
 
 
@@ -175,3 +175,4 @@ let regexp_c = "0"
 let regexp_d = "0"
 
 let regexp_e = "0"
+ *)
