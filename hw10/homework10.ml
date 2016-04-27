67941e1d5a1b58f380e445b2c0bf427d73ba4aa9
srli <c>
2016-04-27 00:59:22 -0400

    prelim hw10

---
+++ hw10/homework10.ml
@@ -0,0 +1,158 @@
+(*
+
+HOMEWORK 10
+
+Name:
+
+Email:
+
+Remarks, if any:
+
+*)
+
+
+(*
+ *
+ * Please fill in this file with your solutions and submit it
+ *
+ * The functions below are stubs that you should replace with your
+ * own implementation.
+ *
+ * Always make sure you can #use this file before submitting it.
+ * Do that in a _fresh_ OCaml shell
+ * It has to load without any errors.
+ *
+ *)
+
+
+type 'a bintree =
+  | Empty
+  | Node of 'a * 'a bintree * 'a bintree
+
+
+let sample = Node(10,Node(3,Node(7,Empty,Empty),
+                            Node(5,Empty,Empty)),
+                     Node(6,Node(99,Empty,
+                                 Node(66,Empty,Empty)),
+                          Empty))
+
+
+(* Printing an integer binary tree *)
+
+let pbt bt =
+  let rec loop bt depth =
+    match bt with
+    | Empty -> ()
+    | Node(n,left,right) ->
+	(loop right (depth^"    ");
+         print_endline (depth^(string_of_int n));
+         loop left (depth^"    ")) in
+  loop bt ""
+
+
+(* Q1 *)
+
+let rec size t =
+  let rec loop t depth =
+  match t with
+  | Empty -> 0
+  | Node(n, left, right) ->
+    (loop right depth)+(loop left depth)+1
+  in loop t ""
+
+
+let rec sum t =
+  let rec loop t depth =
+  match t with
+  | Empty -> 0
+  | Node(n, left, right) ->
+    (loop right depth)+(loop left depth)+n
+  in loop t ""
+
+let rec height t =
+  let rec loop t depth =
+  match t with
+  | Empty -> 0
+  | Node(n, left, right) ->
+    1 + (max (loop right depth) (loop left depth))
+  in loop t ""
+
+let rec fringe t =
+  match t with
+  | Empty -> []
+  | Node(n, left, right) ->
+    if (left = Empty) then
+      if (right = Empty) then
+        [n]
+      else
+        (fringe left)@fringe(right)
+    else
+      (fringe left)@fringe(right)
+
+let rec map f t =
+  match t with
+  | Empty -> Empty
+  | Node(n, left, right) ->
+      Node((f n), (map f left), (map f right))
+
+let rec fold f t b =
+  match t with
+  | Empty -> b
+  | Node(n, left, right) ->
+    f n (fold f left b) (fold f right b)
+
+let preorder t =
+  let pre_o = (fun v l r -> v::l@r) in
+    fold pre_o t []
+
+let postorder t =
+  let pos_o = (fun v l r -> l@r@[v]) in
+    fold pos_o t []
+
+let inorder t =
+  let ino_o = (fun v l r -> l@[v]@r) in
+    fold ino_o t []
+
+let rec bst_insert t x =
+  match t with
+  | Empty -> Node(x, Empty, Empty)
+  | Node(n, left, right) ->
+    if n < x then
+      if (right = Empty) then
+        Node(n, Empty, Node(x, Empty, Empty))
+      else if (List.hd (preorder right)) > x then
+        Node(n, Empty, Node(x, Empty, right))
+      else
+        Node(n, left, (bst_insert right x))
+    else if n > x then
+      if (left = Empty) then
+        Node(n, Node(x, Empty, Empty), Empty)
+      else if (List.hd (preorder left)) < x then
+        Node(n, Empty, Node(x, left, Empty))
+      else
+        Node(n, (bst_insert left x), right)
+    else
+      Node(n, left, right)
+
+let rec bst_lookup t x =
+  match t with
+  | Empty -> false
+  | Node(n, left, right) ->
+    if n = x then
+      true
+    else if n > x then
+      bst_lookup left x
+    else
+      bst_lookup right x
+
+let rec bstify_helper t nodes =
+  match nodes with
+  | [] -> Empty
+  | hd :: tl ->
+    bstify_helper (bst_insert t hd) tl
+
+let bstify t =
+  bstify_helper Empty (preorder t)
+
+
+let avl_insert t x = failwith ("avl_insert not implemented")
